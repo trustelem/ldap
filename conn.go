36b4938c595154a7f18ddd5ae61cb9536a642b20
Hossein Ahmadian-Yazdi <hahmadia@users.noreply.github.com>
2019-11-29 18:35:29 -0500

    Fix printing packets references (#250)

--- conn.go
+++ conn.go
@@ -291,7 +291,7 @@ func (l *Conn) StartTLS(config *tls.Config) error {
 			l.Close()
 			return err
 		}
-		ber.PrintPacket(packet)
+		l.Debug.PrintPacket(packet)
 	}
 
 	if err := GetLDAPError(packet); err == nil {
@@ -505,7 +505,7 @@ func (l *Conn) processMessages() {
 					msgCtx.sendResponse(&PacketResponse{message.Packet, nil})
 				} else {
 					log.Printf("Received unexpected message %d, %v", message.MessageID, l.IsClosing())
-					ber.PrintPacket(message.Packet)
+					l.Debug.PrintPacket(message.Packet)
 				}
 			case MessageTimeout:
 				// Handle the timeout by closing the channel
